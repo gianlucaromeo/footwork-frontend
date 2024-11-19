b67a428ebab0cab9aabb3fa0a2c56201ba17fff5
gianlucaromeo <gianlucaromeo@outlook.com>
2024-11-19 12:26:43 +0200

    Added pages

--- footwork-frontend/src/App.jsx
+++ footwork-frontend/src/App.jsx
@@ -1,6 +1,18 @@
-function App() {
+import { Routes, Route } from 'react-router-dom'
+import AuthenticationPage from './pages/AuthenticationPage'
+import RegistrationFinalStepPage from './pages/RegistrationFinalStepPage'
+import LoginPage from './pages/LoginPage'
+import RegistrationPage from './pages/RegistrationPage'
 
-  return (<></>)
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<AuthenticationPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/registration" element={<RegistrationPage />} />
+      <Route path="/registration-final-step" element={<RegistrationFinalStepPage />} />
+    </Routes>
+  )
 }
 
 export default App
