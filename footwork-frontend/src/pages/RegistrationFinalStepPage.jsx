ea16b4ee622d93e03b87dea65cd979e65dc97b4f
gianlucaromeo <gianlucaromeo@outlook.com>
2024-11-20 20:20:00 +0200

    Removed unnecessary file

--- footwork-frontend/src/pages/RegistrationFinalStepPage.jsx
+++
@@ -1,9 +0,0 @@
-const RegistrationFinalStepPage = () => {
-    return (
-        <div>
-        <h1>Registration Final Step</h1>
-        </div>
-    );
-}
-
-export default RegistrationFinalStepPage
