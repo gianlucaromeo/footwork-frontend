fd27563637951c37ed7af73cfab043eafa34f80e
Simone Kinders <simonekinders@gmail.com>
2024-11-21 22:45:27 +0200

    Made changes is VideoRow to make it clickable

--- footwork-frontend/src/components/VideoRow.jsx
+++ footwork-frontend/src/components/VideoRow.jsx
@@ -1,53 +1,41 @@
 import React from 'react';
-import deleteIcon from '../assets/icons/delete-white.png'; // Importing the icon from assets
+import deleteIcon from '../assets/icons/delete-black.png'; 
 
-const VideoRow = ({ 
-    videoNumber = "",  // Video number (e.g., "1")
-    thumbnail,         // Thumbnail image source
-    title = "",        // Title of the video
+const VideoRow = ({
+    videoNumber = "",      // Video number (e.g., "1")
+    thumbnail,             // Thumbnail image source
+    title = "",            // Title of the video
     iconAlt = "Delete Icon", // Alt text for the icon
+    onClick,               // Callback for row click
+    onDelete,              // Callback for delete icon click
 }) => {
-  return (
-    <div 
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        padding: '1rem',
-        border: '1px solid #ddd',
-        borderRadius: '5px',
-        marginBottom: '1rem',
-      }}
-    >
-      {/* Compartment 1: Video Number */}
-      <div style={{ width: '50px', textAlign: 'center' }}>
-        {videoNumber}
-      </div>
-
-      {/* Compartment 2: Thumbnail */}
-      <div>
-        <img
-          src={thumbnail}
-          alt="Video Thumbnail"
-          style={{ width: '60px', height: '60px', borderRadius: '5px', objectFit: 'cover' }}
-        />
-      </div>
-
-      {/* Compartment 3: Title */}
-      <div style={{ flex: 1, marginLeft: '1rem' }}>
-        <strong>{title}</strong>
-      </div>
-
-      {/* Compartment 4: Delete Icon */}
-      <div>
-        <img
-          src={deleteIcon}
-          alt={iconAlt}
-          style={{ width: '24px', height: '24px', cursor: 'pointer' }}
-        />
-      </div>
-    </div>
-  );
+    return (
+        <button
+            onClick={onClick} // Handle row click
+        >
+            <div>
+                {videoNumber}
+            </div>
+            <div>
+                <img
+                    src={thumbnail}
+                    alt="Video Thumbnail"
+                />
+            </div>
+            <div>
+                {title}
+            </div>
+            <div onClick={(e) => {
+                e.stopPropagation(); // Prevent row's onClick from triggering
+                onDelete(videoNumber); // Call delete handler
+            }}>
+                <img
+                    src={deleteIcon}
+                    alt={iconAlt}
+                />
+            </div>
+        </button>
+    );
 };
 
 export default VideoRow;
