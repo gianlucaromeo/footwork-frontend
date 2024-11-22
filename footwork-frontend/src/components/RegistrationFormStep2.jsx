1da344233f65442b12bc80f1ee5f93370bcf44d9
Sarah Sommerschuh <so.sarah@freenet.de>
2024-11-22 16:19:10 +0200

    Added CSS to RegistrationFormStep2

--- footwork-frontend/src/components/RegistrationFormStep2.jsx
+++ footwork-frontend/src/components/RegistrationFormStep2.jsx
@@ -9,26 +9,43 @@ const RegistrationFormStep2 = ({
 }) => {
 
   return (
-    <div>
-      <h3>Register to join Polijazz</h3>
-      <div>Hi {firstName}</div>
-      <div>
-        Nice to meet you and welcome to Polijazz. Do you register as a student or teacher?
+    <div className="registrationForm">
+      <div className="header">
+        <h3>Register to join Polijazz</h3>
       </div>
-      <SwitchButton 
-        nameButtonLeft="student" 
-        nameButtonRight="teacher"
-        contentLeft={<RegistrationCoursesOptions
-          onSelectedCoursesChanged={onSelectedCoursesChanged}
-        />}
-        contentRight="We will notify Polijazz about your access request"
-        onLeftClick={() => onIsStudentChanged(true)}
-        onRightClick={() => onIsStudentChanged(false)}
-      />
-      <div>
-        <button>Back</button>
-        <button onClick={onConfirm}> Confirm</button>
+      <div className="content">
+        <div className="titleDescription">
+          <div className="copy-xlarge-b">Hi {firstName}</div>
+          <div className="copy-medium-reg">Nice to meet you and welcome to Polijazz. Do you register as a student or teacher?</div>
+        </div>
+        <SwitchButton 
+          nameButtonLeft="student" 
+          nameButtonRight="teacher"
+          contentLeft={<RegistrationCoursesOptions
+            onSelectedCoursesChanged={onSelectedCoursesChanged}
+          />}
+          contentRight="We will notify Polijazz about your access request"
+          onLeftClick={() => onIsStudentChanged(true)}
+          onRightClick={() => onIsStudentChanged(false)}
+        />
       </div>
+      <div className="buttonContainer">
+            <Button 
+              className={isMobile ? "btn-text m" : "btn-text s"}
+                // handleContinue not set yet so set to onContinue so that code works!
+                // CHANGE
+                // onClick={handleContinue}
+                onClick={onContinue}
+                disabled={isButtonActive}
+                text="Back"
+            />
+            <Button 
+              className={`${isMobile ? 'btn-primary m' : 'btn-primary s'} ${isButtonActive ? ' enabled' : ''}`}
+              onClick={onConfirm}
+              disabled={!isButtonActive}
+              text="Confirm"
+            />
+          </div>
     </div>
   );
 };
