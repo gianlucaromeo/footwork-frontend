6c80d938429d065dff2a7aa688c97e784bccbba8
Sarah Sommerschuh <so.sarah@freenet.de>
2024-11-23 15:37:47 +0200

    Added CSS SectionStudentReview

---
+++ footwork-frontend/src/components/SectionStudentReview.jsx
@@ -0,0 +1,17 @@
+
+const SectionStudentReview = () => {
+    return (
+            <div className="reviewContainer">
+                <div className="titleSubtitle review">
+                    <h2>Welcome ***NAME</h2>
+                    <p>Your account is under review by a teacher</p>
+                </div>
+                <div className="notification">
+                    <p>You will get notified via email once your status has been updated</p>
+                    <p>This will take between 1 to 3 days</p>
+                </div>
+            </div>
+    );
+};
+
+export default SectionStudentReview;
